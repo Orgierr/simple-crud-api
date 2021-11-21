6c20f10f1b6ae1e359df05318369bfba11bd86ab
Igor.K <infinitemind625@gmail.com>
2021-11-21 16:27:38 +0200

    feat:add custom error and validate url

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,16 @@
 const persons = require('../db/persons');
 
 async function app(req, res) {
-  req.persons = persons;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(persons));
+  try {
+    req.persons = persons;
+    res.setHeader('Content-Type', 'application/json');
+
+    validateUrl(req, res)
+
+  } catch (error) {
+    res.statusCode = error.message.code || 500;
+    res.end(JSON.stringify(error.message || 'Internal Server Error'));
+  }
 }
 
 module.exports = app;
